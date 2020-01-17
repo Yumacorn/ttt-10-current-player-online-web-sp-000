bce7d66f598abfc220dfaeeda879a0c673e6c42a
Ryan Mui <reachryanmui@gmail.com>
2020-01-17 04:24:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turn_number = 0
   board.each do |space|
-    if (board[space] == "X" || board[space] == "O")
+    if space == "X" || space == "O"
       turn_number += 1
     end
   end
