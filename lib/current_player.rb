017b44a7afd6faf94423f2043decc465f4bdede3
Ryan Mui <reachryanmui@gmail.com>
2020-01-17 04:20:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
   turn_number = 0
-  board.each{|space| if (board[space] == "X" || board[space] == "O") then turn_number += 1}
+  board.each{|space| if (board[space] == "X" || board[space] == "O") then turn_number += 1 }
   turn_number
 end
